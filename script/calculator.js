fa7fed7f56e51cd59795c4abbd0a4d73855efa07
Seba L <sebastian.laskowski15@gmail.com>
2023-01-17 22:02:14 +0100

    V0.0.3

--- script/calculator.js
+++ script/calculator.js
@@ -1,4 +1,54 @@
 //Calculator App
-let zmienna;
+const display = document.getElementById('display');
+let a = "";
+let b = "";
+let operator;
+
+function updateDisplay(content) {
+    display.innerHTML = content;
+}
+
+function addContent(element) {
+    if (element == '+' && operator == undefined) {
+        b = a;
+        a = 0;
+        operator = element;
+    }
+    else if (element == '-' && operator == undefined) {
+        b = a;
+        a = 0;
+        operator = element;
+    }
+    else if (element == '/' && operator == undefined) {
+        b = a;
+        a = 0;
+        operator = element;
+    }
+    else if ((a == 0 && operator && element > 0 || element != undefined) || (a == 0 && operator && element > 0 || element == undefined)) a = element;
+    else if (Number(element) > 0 && Number(element) <= 9) a += element;
+       
+    updateDisplay(a);
+}
+
+function equal() {
+    if (operator == '+') {
+        updateDisplay(Number(b) + Number(a));
+        a = 0;
+        b = 0;
+        operator = undefined;
+    }
+    else if (operator == '-') {
+        updateDisplay(Number(b) - Number(a));
+        a = 0;
+        b = 0;
+        operator = undefined;
+    }
+    else if (operator == '/') {
+        updateDisplay(Number(b) / Number(a));
+        a = 0;
+        b = 0;
+        operator = undefined;
+    }
+}
 
 
