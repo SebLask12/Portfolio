17fdace4aaf7cebf3c3fe6db6d4cf08b819362c7
Seba L <sebastian.laskowski15@gmail.com>
2023-01-24 20:15:21 +0100

    v0.0.9 - implement navigation

--- script/script.js
+++ script/script.js
@@ -2,6 +2,17 @@ const link = document.querySelectorAll('.navigation__link');
 const link_array = [...link];
 const checkbox = document.getElementById('navi-toggle');
 
+document.querySelectorAll('.navigation__link').forEach(function (el) {
+    el.addEventListener('click', function (e) {
+        e.preventDefault();
+        checkbox.checked = false;
+        const id = this.getAttribute('href');
+        document.querySelector(id).scrollIntoView({
+            behaviour: 'smooth'
+        })
+    })
+});
+
 checkbox.addEventListener('click', function (e) {
     if (checkbox.checked == true) {
         link_array.forEach(navigation__link => {
